2e08f98c083a79f70f64c61fc884d3890d9cac8e
Dewi-Payne <dewitbpayne@gmail.com>
2024-03-28 15:40:29 +0000

    added auto fail test to check jenkins

--- src/main/java/testCases/TestCase01.java
+++ src/main/java/testCases/TestCase01.java
@@ -1,5 +1,13 @@
 package testCases;
 
+import static org.junit.Assert.assertEquals;
+
+import io.cucumber.java.Before;
+
 public class TestCase01 {
+	@Before
+	private static void setUp() {
+		assertEquals(true, false);	
+	}
 	
 }
